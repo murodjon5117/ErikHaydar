fd36ca5a115869dc5e065d9fe286dd7ffdac2f76
Yuldoshev <i.yuldoshev93@gmail.com>
2022-10-31 07:04:11 +0500

    create home

--- lib/view/sceen/home/home_categories.dart
+++ lib/view/sceen/home/home_categories.dart
@@ -1,7 +1,5 @@
 import 'package:erik_haydar/util/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../util/images.dart';
